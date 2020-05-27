a818f88357df3c600a4dd1b99fe4358790d2ba28
HelmuthBecker <54779926+HelmuthBecker@users.noreply.github.com>
2020-05-27 19:18:07 -0300

    Atualização de Firmware para Módulos Emissores
    Refeito código do emissor para suportar módulos de 1W/8km "EBYTE E32 433T30D" e 100mW/3km "EBYTE E32 433T20D"
    Implementada função que gera um percurso fake com dados de latitude e longitude fictícios, para fins de teste com a aplicação de monitoramento.
    Código referente ao uso com módulo SIM 808 movido para outro repositório

--- track_Sender/track_Sender.ino
+++ track_Sender/track_Sender.ino
@@ -1,250 +1,103 @@
 /*
- 
- SD card attached to SPI bus as follows:                  SoftSerial                        Objeto EBYTE                      LED de mensagem enviada/recebida
-  ** MOSI - pin 11                                        ** 2,3 - GPS Rx, Tx               ** 7, 8, 9 - M0, M1, AUX          ** 4 - LED verde
-  ** MISO - pin 12                                        ** 5,6 - LORA Rx, Tx      
-  ** CLK - pin 13
-  ** CS - pin 10 (for MKRZero SD: SDCARD_SS_PIN)
+
+                          **************SKETCH PARA MÓDULOS DE 1W/8km "EBYTE E32 433T30D" E 100mW/3km "EBYTE E32 433T20D"**************
+                              ***SUBSTITUIR O VALOR DO PARÂMETRO "SetTransmitPower" NA FUNÇÃO "INICIAR LORA" CONFORME NECESSÁRIO***
+                          
+                   SoftSerial (Saídas Digitais)      Objeto EBYTE (Saídas Digitais)    
+                   ** 3,2 -  RX,TX  (GPS)           ** 7, 8, 10 - M0, M1, AUX(Definido mas não conectado)          
+                   ** 9,6 -  RX,TX (LORA)                                                
   
  */
- 
-//#include <SPI.h>
-#include <SoftwareSerial.h> //Biblioteca SoftwareSerial
-//#include <SD.h> //Biblioteca SD
-#include <EBYTE.h> //Biblioteca dos módulos LORA
 
-SoftwareSerial serialGPS(2,3); //Rx-Tx
-SoftwareSerial serialLORA(5,6); //Rx-TX)
+#include <EBYTE.h>
+#include <SoftwareSerial.h>
 
 #define DEBUG true
-//#define SD_CARD_CS      10
 
-String NUMERO_BARCO = "7";                            // Número da embarcação
-String response;                                      //Contém a informação bruta, enviada pelo módulo GPS, após o comando AT.
-int    tamanhoString;                                 //Informa a qtd de char da variável response; Acima de 110 char, indica que os dados fornecidos pelo GPS são utilizaveis.
-String data[7];                                       //Vetor de string que, após separados os dados relevantes existentes na variável response, 
-                                                      //irá armazenar os valores referentes a Lat, Long, Time e status separadamente.
-String pacoteMensagem;                                //Contém o pacote de mensagem a ser enviado pelo módulo LORA
-//File   myFile;                                      //Arquivo de LOG do SD
+SoftwareSerial mySerial(9,6); //Rx - Tx (LORA)
 
-EBYTE emissor(&serialLORA, 7, 8, 9);                  //Objeto EBYTE - parâmetros PINOS Rx,Tx, M0, M1, AUX
+EBYTE emissor(&mySerial, 7, 8, 10); //Parâmetros do módulo LORA (RX,TX,M0,M1,AUX)
 
-void setup()
-{
+void setup() {
   
-   pinMode(4, OUTPUT);                               //LED verde de confirmação de recebimento de mensagem pelo módulo receptor
-   digitalWrite(4, LOW);
-      
-   Serial.begin     (9600);
-   serialGPS.begin  (9600);
-   serialLORA.begin (9600);
-  
- /*
-   if (!SD.begin(10)) {
-   Serial.println("Falha ao iniciar SD!!!\nDesligue o arduino e insira um cartão SD!");
-   delay (3000);
-   while (1);
-   }
-   Serial.println("Cartão SD iniciado");
-*/
+Serial.begin(9600); //Inicia a serial física
+mySerial.begin(9600); //Inicia a serial lógica
 
- Serial.println("Iniciando GPS...");
-   while (response.length() != 21)                     //Analisa o tamanho da string de resposta do módulo GPS, caso seja 21 prossegue com o sketch
-   {                     
-    Serial.println("Iniciando GPS...");
-    getgps();                                          //Função com comandos para iniciar o GPS.
-    delay (3000);
-   }  
- Serial.println("GPS iniciado");
-
- iniciarLORA();
+iniciarLORA(); //Função que passa os parâmetros de funcionamento do módulo LORA
 }
 
-      void loop()
-       {
-        serialGPS.listen();
-        
-        sendData("AT+CGNSINF",1000,DEBUG);            //Envia comando ao módulo solicitando dados do GPS
-        criarMensagem();                              //Faz o tratamento da string recebida pelo módulo com todos dados fornecidos
-        if (data[1] != 0)                             //Se o valor da variavel status enviado pelo GPS for diferente de 0, indica que os dados recebidos estão OK
-         {                            
-          while (data[1] != 0)                        //Enquanto o status for diferente de 0, permanece no loop enviando o comando para obter info, recebendo a 
-           {                                          //string de resposta, salvando no SD, exibindo na serial, e enviando via LORA
-            sendData("AT+CGNSINF",1000,DEBUG);  
-            delay(1000);
-            criarMensagem();
-            enviarLORA();
-            confirmaRecebimento(5000);
-            //gravarSD();
-            // delay(1000);
-           }
-        }
-         else 
-          {                                      //Caso o módulo perca o sinal com o GPS, tenta reconectar
-           getgps();
-           Serial.println("Aguardando Sinal GPS...");
-           delay(5000);
-          }       
-        }
+void loop() 
+ {  
+             percursoFake(); //Função que gera um percurso FAKE (apenas para testes temporariamente)
+ }
 
-      void iniciarLORA () 
-       {
-         serialLORA.listen();                   
+
+ void iniciarLORA () 
+  {
+     mySerial.listen();                   
        
-         emissor.init();                                        //Inicia o módulo
-/*
-         emissor.SetMode(MODE_NORMAL);                          //Modo de funcionamento do módul
-         emissor.SetAddressH(0);                                //Endereço H(?)
-         emissor.SetAddressL(0);                                //Endereço L(?)
-         emissor.SetAirDataRate(ADR_2400);                      //AirDataRate 2400kbps
-         emissor.SetUARTBaudRate(UDR_9600);                     //BAUDRate 9600
-         emissor.SetChannel(23);                                //Canal 23
-         emissor.SetParityBit(PB_8N1);                          //Bit Paridade 8N1
-         emissor.SetTransmitPower(OPT_TP30);                    //Força de transmissão 30db
-         emissor.SetWORTIming(OPT_WAKEUP2000);                  //WakeUP Time(?) 2000
-         emissor.SetFEC(OPT_FECENABLE);                         //FEC(?)
-         emissor.SetOptionTrans(OPT_OPTIONTRANSDISABLE);
-         emissor.SetIOMode(OPT_IOPUSHPULL);                     //IO Mode PushPull
-         emissor.SaveParameters(PERMANENT);                     //Salva as modificações na memória do módulo
-*/         
-         emissor.PrintParameters();                             //Exibe os parâmetros configurados
-      }
+     emissor.init();                                            //Inicia o módulo
 
-    void confirmaRecebimento(const int timeout)                 //Função que aguarda por um char de resposta do receptor para piscar o LED
-     {
-      serialLORA.listen();
-      long int time = millis();
-      
-      Serial.println("Aguardando resposta");
-        
-      while((time+timeout) > millis())
-       {
-        while (serialLORA.available())
-         {
-          char c = serialLORA.read();
-          if (c == '1')
-           {
-            digitalWrite(4, HIGH);
-            delay(500);
-            digitalWrite(4, LOW);
-            delay(500);
-            digitalWrite(4, HIGH);
-            delay(500);
-            digitalWrite(4, LOW);
-            break;
-          }
-        }
-      }
-    Serial.println("FIM");
-   }
-  
-  void enviarLORA()                                                                                   //Função que envia o pacote com os dados do GPS
-   {
-    serialLORA.listen();
-    
-    if (pacoteMensagem.length() == 49 && pacoteMensagem[0] == '[' && pacoteMensagem[48] == ']' )      //Verifica se a string está inteira e consistente antes do envio
-     {
-      serialLORA.println (pacoteMensagem);
-      serialLORA.println("OK");
-     }
-   }
-   
-/*
-  void gravarSD()
-   {
-    //abre o arquivo para edição
-    File myFile = SD.open("dados.txt", FILE_WRITE);
-      if (myFile) 
-       {
-        Serial.println(pacoteMensagem);
-        Serial.print("Writing to test.txt...");
-        //grava o que foi digitado no arquivo
-        myFile.println(pacoteMensagem);
-        //fecha e salva o arquivo após edição
-        myFile.close();
-        Serial.println("done.");
-       } else 
-         {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
-         } 
-    }*/
-   
-   void criarMensagem ()                            //Função que faz o tratamento da string com os dados do GPS
-    {                                    
-     int i = 0;                                     //contador da string data
-     tamanhoString = response.length();             //Informa a qtd de char da variável response; Acima de 110 char, indica que os dados fornecidos pelo GPS são utilizaveis.
-     Serial.println();
-     if (tamanhoString > 110)                       //indica que os dados fornecidos pelo GPS são utilizaveis.
-      {                     
-       for (byte a = 1; a < tamanhoString; a++)     //Enquanto o contador a for menor que o tamanho total da string, executa o loop para tratamento de response
-        { 
-         char h = response[a];                      //char h, variavel que armazena o valor da string response byte a byte
-         if (h != ',')                              //enquanto h for diferente de "," preenche a string
-          {    
-           data[i] +=h;
-           h = "";                                   //esvazia a variavel para o próximo loop
-           delay(100);
-          } else 
-             {                                      //caso h seja igual "," incrementa o contador i o que faz criar uma nova string para armazenar dados da variavel response
-              i++;  
-             }
-               if (i == 7)                          //quando o contador i chega a 7, todas variáveis com os dados relevantes do GPS estão criadas.
-                {   
-                 pacoteMensagem = "["+NUMERO_BARCO+","+data[2]+","+data[3]+","+data[4]+","+data[6]+"]";
+     emissor.SetMode(MODE_NORMAL);                              //Modo de funcionamento do módulo
+     emissor.SetAddressH(0);                                    //Endereço H(?)
+     emissor.SetAddressL(0);                                    //Endereço L(?)
+     emissor.SetAirDataRate(ADR_2400);                          //AirDataRate 2400kbps
+     emissor.SetUARTBaudRate(UDR_9600);                         //BAUDRate 9600
+     emissor.SetChannel(23);                                    //Canal 23
+     emissor.SetParityBit(PB_8N1);                              //Bit Paridade 8N1
+     emissor.SetTransmitPower(OPT_TP30);                        //Força de transmissão 30db (Para módulos de 1W/8km definir como 30)
+     //emissor.SetTransmitPower(OPT_TP20);                      //Força de transmissão 20db (Para módulos 100mW/3km definir como 20)
+     emissor.SetWORTIming(OPT_WAKEUP250);                       //WakeUP Time(?) 2000
+     emissor.SetFECMode(OPT_FECENABLE);                         //FEC(?) ENABLE
+     emissor.SetTransmissionMode(OPT_FMDISABLE);                //Transmission Mode
+     emissor.SetPullupMode(OPT_IOPUSHPULL);                     //IO Mode PushPull
+     emissor.SaveParameters(PERMANENT);                         //Salva as modificações na memória do módulo
+         
+     emissor.PrintParameters();                                 //Exibe os parâmetros configurados
+  } //Fim iniciarLORA 
 
-                 Serial.println("Barco Nº: "+NUMERO_BARCO);
-                 Serial.println("Status: "     +data[1]);
-                 Serial.println("Time: "       +data[2]);
-                 Serial.println("Latitude: "   +data[3]);
-                 Serial.println("Longitude: "  +data[4]);
-                 Serial.println("Velocidade: " +data[6]+" Nós");
-                 Serial.println(pacoteMensagem);
-                 break;
-                }
-        }
-     }    
-   }
-  
-      void getgps(void)                                   //Função que ativa o GPS
-       {
-        sendData( "AT+CGNSPWR=1",1000,DEBUG);            //Liga o GPS em modo GNSS
-        Serial.println(response); 
-        sendData( "AT+CGNSSEQ=RMC",1000,DEBUG);          //Parâmetro de configuração
-        Serial.println(response); 
-      }
-      
-     String sendData(String command, const int timeout, boolean debug)
-      {
-       serialGPS.listen();
+  void percursoFake() //Função que gera um percurso FAKE (apenas para testes temporariamente)
+ {
+
+   int vel = 100; //Define de quanto em quanto o loop irá avançar até atingir o valor limite definido no while
+   int LO = 4660; //Valores de longitude
+   int NS = 2370; //Valores de latitude
+
+   while (LO > 1880)
+    {
+     LO -= vel ;
+     //mySerial.print("[06,-26.242370,-48.64"+(String)LO+"]*"); //Para cada equipe, substituir de acordo, para que os módulos não enviem informações idênticas
+     //mySerial.print("[07,-26.242370,-48.64"+(String)LO+"]*");
+     mySerial.print("[08,-26.242370,-48.64"+(String)LO+"]*");
+     delay(random(1000, 1500));
+    }
+   
+   while (NS < 5150)
+    {
+     LO = 1880;
+     NS += vel;
+     //mySerial.print("[06,-26.24"+(String)NS+",-48.641880]*");
+     //mySerial.print("[07,-26.24"+(String)NS+",-48.641880]*");
+     mySerial.print("[08,-26.24"+(String)NS+",-48.641880]*");
+     delay(random(1000, 1500));
+    }
     
-        //Efetua a limpeza das variáveis antes de executar a função
-        data[0]="";                                     //Informações irrelevantes
-        data[1]="";                                     //Status do GPS
-        data[2]="";                                     //Time
-        data[3]="";                                     //Latitude
-        data[4]="";                                     //Longitude
-        data[5]="";                                     //Informações irrelevantes
-        data[6]="";                                     //Velocidade Nós
-        data[7]="";                                     //Informações irrelevantes
-        response = "";                                  //Informações completas do módulo GPS
-        pacoteMensagem = "";
+   while (LO < 4660)
+    {
+     NS = 5150;
+     LO += vel;
+     //mySerial.print("[06,-26.245150,-48.64"+(String)LO+"]*");
+     //mySerial.print("[07,-26.245150,-48.64"+(String)LO+"]*");
+     mySerial.print("[08,-26.245150,-48.64"+(String)LO+"]*");
+     delay(random(1000, 1500));
+    }
     
-        serialGPS.println(command); 
-        
-        long int time = millis();   
-    
-        while( (time+timeout) > millis())               //Enquanto a soma de time(millis()) + timeout for maior do que o valor atual de millis(), lê caracteres da Serial.
-         {
-          while(serialGPS.available())
-           {       
-            char c = serialGPS.read(); 
-            response+=c;
-           }  
-         }    
-          if(debug)                                       //Se debug for true, retorna o valor da variavel response
-           {
-            //Serial.print(response);
-            return response;
-           }
-     }
+   while (NS > 2370)
+    {
+     NS -= vel;
+     //mySerial.print("[06,-26.24"+(String)NS+",-48.644660]*");
+     //mySerial.print("[07,-26.24"+(String)NS+",-48.644660]*");
+     mySerial.print("[08,-26.24"+(String)NS+",-48.644660]*");
+     delay(random(1000, 1500));
+    }
+}
