96c353356bb29c002c7e38969c039191a9a4fa32
HelmuthBecker <54779926+HelmuthBecker@users.noreply.github.com>
2020-06-05 20:03:36 -0300

    Sketch do emissor
    Sketch para módulos emissores, que utilizam uma forma diferente de funcionamento, onde o receptor solicita os dados a um emissor por vez, diferente do modo antigo, onde o receptor recebia as informações todas ao mesmo tempo sem controle.

---
+++ MODULO SOLICITA UM POR UM/enviar/enviar.ino
@@ -0,0 +1,165 @@
+/*
+
+                          **************SKETCH PARA MÓDULOS DE 1W/8km "EBYTE E32 433T30D" E 100mW/3km "EBYTE E32 433T20D"**************
+                              ***SUBSTITUIR O VALOR DO PARÂMETRO "SetTransmitPower" NA FUNÇÃO "INICIAR LORA" CONFORME NECESSÁRIO***
+                          
+                   SoftSerial (Saídas Digitais)      Objeto EBYTE (Saídas Digitais)    
+                   ** 3,2 -  RX,TX  (GPS)           ** 7, 8, 10 - M0, M1, AUX(Definido mas não conectado)          
+                   ** 9,6 -  RX,TX (LORA)                                                
+  
+ */
+
+#include <EBYTE.h>
+#include <SoftwareSerial.h>
+
+
+char mensagem[5];  //Array de caracteres que irá armazenar a mensagem a ser recebida
+size_t bytesRecebidos;
+
+SoftwareSerial mySerial(9,6); //Rx - Tx (LORA)
+
+EBYTE emissor(&mySerial, 7, 8, 10); //Parâmetros do módulo LORA (RX,TX,M0,M1,AUX)
+
+void setup() {
+  
+Serial.begin(9600); //Inicia a serial física
+mySerial.begin(9600); //Inicia a serial lógica
+
+iniciarLORA(); //Função que passa os parâmetros de funcionamento do módulo LORA
+}
+
+void loop() 
+ {  
+             percursoFake(); //Função que gera um percurso FAKE (apenas para testes temporariamente)
+ }
+
+
+ void iniciarLORA () 
+  {
+     mySerial.listen();                   
+       
+     emissor.init();                                            //Inicia o módulo
+
+     emissor.SetMode(MODE_NORMAL);                              //Modo de funcionamento do módulo
+     emissor.SetAddressH(0);                                    //Endereço H(?)
+     emissor.SetAddressL(0);                                    //Endereço L(?)
+     emissor.SetAirDataRate(ADR_2400);                          //AirDataRate 2400kbps
+     emissor.SetUARTBaudRate(UDR_9600);                         //BAUDRate 9600
+     emissor.SetChannel(23);                                    //Canal 23
+     emissor.SetParityBit(PB_8N1);                              //Bit Paridade 8N1
+     //emissor.SetTransmitPower(OPT_TP30);                        //Força de transmissão 30db (Para módulos de 1W/8km definir como 30)
+     emissor.SetTransmitPower(OPT_TP20);                      //Força de transmissão 20db (Para módulos 100mW/3km definir como 20)
+     emissor.SetWORTIming(OPT_WAKEUP250);                       //WakeUP Time(?) 2000
+     emissor.SetFECMode(OPT_FECENABLE);                         //FEC(?) ENABLE
+     emissor.SetTransmissionMode(OPT_FMDISABLE);                //Transmission Mode
+     emissor.SetPullupMode(OPT_IOPUSHPULL);                     //IO Mode PushPull
+     emissor.SaveParameters(PERMANENT);                         //Salva as modificações na memória do módulo
+         
+     emissor.PrintParameters();                                 //Exibe os parâmetros configurados
+  } //Fim iniciarLORA 
+
+void percursoFake() //Função que gera um percurso FAKE (apenas para testes temporariamente)
+ {
+
+   int vel = 100; //Define de quanto em quanto o loop irá avançar até atingir o valor limite definido no while
+   int LO = 4660; //Valores de longitude
+   int NS = 2370; //Valores de latitude
+   String dados;
+   unsigned long pausa;
+   
+   while (LO > 1880)
+    {
+     pausa = millis() + 3000;
+     LO -= vel ;
+     //dados = "[06,-26.242370,-48.64"+(String)LO+"]*";
+     //dados = "[07,-26.242370,-48.64"+(String)LO+"]*"; //Para cada equipe, substituir de acordo, para que os módulos não enviem informações idênticas
+     dados = "[08,-26.242370,-48.64"+(String)LO+"]*";
+     Serial.println(dados);
+        
+     while (pausa > millis()){
+        receber();
+        if (mensagem[0] == 'I' ){
+        //if (mensagem[0] == 'S' ){
+        //if (mensagem[0] == 'A' ){
+          mySerial.print(dados);
+          break;
+          }
+        }
+      }
+   
+   while (NS < 5150)
+    {
+     pausa = millis() + 3000;
+     LO = 1880;
+     NS += vel;
+     //dados = "[06,-26.24"+(String)NS+",-48.641880]*";
+     //dados = "[07,-26.24"+(String)NS+",-48.641880]*";
+     dados = "[08,-26.24"+(String)NS+",-48.641880]*";
+     Serial.println(dados);
+     
+     while (pausa > millis()){
+        receber();
+        if (mensagem[0] == 'I' ){
+        //if (mensagem[0] == 'S' ){
+        //if (mensagem[0] == 'A' ){
+          mySerial.print(dados);
+          break;
+          }
+       }
+     }
+    
+   while (LO < 4660)
+    {
+      pausa = millis() + 3000;
+     NS = 5150;
+     LO += vel;
+
+     //dados = "[06,-26.245150,-48.64"+(String)LO+"]*";
+     //dados = "[07,-26.245150,-48.64"+(String)LO+"]*";
+     dados = "[08,-26.245150,-48.64"+(String)LO+"]*";
+     Serial.println(dados);
+     
+     while (pausa > millis()){
+        receber();
+        if (mensagem[0] == 'I' ){
+        //if (mensagem[0] == 'S' ){
+        //if (mensagem[0] == 'A' ){
+          mySerial.print(dados);
+          break;
+          }
+       }
+    }
+    
+   while (NS > 2370)
+    {
+      pausa = millis() + 3000;
+     NS -= vel;
+
+    //dados = "[06,-26.24"+(String)NS+",-48.644660]*";
+     //dados = "[07,-26.24"+(String)NS+",-48.644660]*";
+     dados = "[08,-26.24"+(String)NS+",-48.644660]*";
+     Serial.println(dados);
+
+     while (pausa > millis()){
+        receber();
+        if (mensagem[0] == 'I' ){
+        //if (mensagem[0] == 'S' ){
+        //if (mensagem[0] == 'A' ){
+          mySerial.print(dados);
+          break;
+       }
+     }
+   }
+
+ }
+ 
+void receber() //Função responsavel por receber as mensagens de solicitação do módulo receptor
+  {
+  //Limpa a variavel mensagem, antes do uso
+     for (byte a = 0; a < 4; a++ ){
+       mensagem[a] = "";
+     }
+     while (mySerial.available()){ //Se houver dados a receber pela serial lógica
+       bytesRecebidos = mySerial.readBytesUntil('*', mensagem, 4);//Recebe o caracter e o atribui a variavek mensagem
+    }
+  }
