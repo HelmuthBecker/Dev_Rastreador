7dc37550848638b1cd9cfb01330ead7a17c92262
HelmuthBecker <54779926+HelmuthBecker@users.noreply.github.com>
2020-06-15 22:07:39 -0300

    Adição e modificação de funções
    Adicionada funcionalidade que permite gravar o LOG das equipes no cartão SD
    Função receberEnviar dividida em duas funções específicas: Função Receber e Função Publicar
    Melhorias em algumas partes do código visando melhor desempenho

--- MODULO SOLICITA UM POR UM/receber/receber.ino
+++ MODULO SOLICITA UM POR UM/receber/receber.ino
@@ -18,19 +18,18 @@
 #include <PubSubClient.h>
 #include <EBYTE.h>
 #include <SoftwareSerial.h>
+#include <SD.h>
 
-#define ARDUINO_CLIENT_ID "ARDUINO"           // Client ID for Arduino pub/sub
-#define PUB_8 "/app/dados/equipe8"            // Tópico MQTT para equipe 8
-#define PUB_9 "/app/dados/equipe9"            // Tópico MQTT para equipe 9
-#define PUB_10 "/app/dados/equipe10"            // Tópico MQTT para equipe 10
+#define ARDUINO_CLIENT_ID "ARDUINO"      // Client ID for Arduino pub/sub
 
 #define LED1 9
 #define LED2 8
 #define LED3 7
 
-char mensagem[27];  //Array de caracteres que irá armazenar a mensagem a ser recebida
+char mensagem[27];                      //Array de caracteres que irá armazenar a mensagem a ser recebida
 size_t bytesRecebidos;
-SoftwareSerial mySerial(3,6);                 //Rx - Tx
+SoftwareSerial mySerial(3,6);           //Rx - Tx
+File myFile;
 
 EBYTE emissor(&mySerial, 5, 4, 2);
 
@@ -49,14 +48,18 @@ void setup() {
    pinMode(LED2, OUTPUT);
    pinMode(LED3, OUTPUT);
 
-   Serial.begin(9600); //Inicia a serial física
-   mySerial.begin(9600); //Inicia a serial lógica
-
+   Serial.begin(9600);                //Inicia a serial física
+   mySerial.begin(9600);              //Inicia a serial lógica
      
-     client.setServer(server, 1883);  // Endereço IP e porta do servidor MQTT
+   client.setServer(server, 1883);    //Endereço IP e porta do servidor MQTT
  
-     
-     Ethernet.begin(mac, ip); // Inicia o Ethernet shield
+   Ethernet.begin(mac, ip);           //Inicia o Ethernet shield
+
+  if (!SD.begin(8)) {
+    Serial.println("Erro SD");
+    while (1);
+  }
+  Serial.println("SD iniciado");
 
    iniciarLORA();
 }
@@ -85,7 +88,7 @@ void iniciarLORA ()   //Função que define os parâmetros de configuração do 
      emissor.SetParityBit(PB_8N1);                              //Bit Paridade 8N1
      emissor.SetTransmitPower(OPT_TP30);                        //Força de transmissão 30db (Para módulos de 1W/8km)
      //emissor.SetTransmitPower(OPT_TP20);                        //Força de transmissão 20db (Para módulos 100mW/3km)
-     emissor.SetWORTIming(OPT_WAKEUP250);                      //WakeUP Time(?) 2000
+     emissor.SetWORTIming(OPT_WAKEUP250);                       //WakeUP Time(?) 2000
      emissor.SetFECMode(OPT_FECENABLE);                         //FEC(?) ENABLE
      emissor.SetTransmissionMode(OPT_FMDISABLE);                //Transmission Mode
      emissor.SetPullupMode(OPT_IOPUSHPULL);                     //IO Mode PushPull
@@ -94,20 +97,17 @@ void iniciarLORA ()   //Função que define os parâmetros de configuração do 
      emissor.PrintParameters();                                 //Exibe os parâmetros configurados
     } //Fim iniciarLORA 
 
-void reconectar() //Função que reconecta caso a conexão com o MQTT seja perdida
+void reconectar()                   //Função que reconecta caso a conexão com o MQTT seja perdida
   {
      // Loop até reconectar
-     while (!client.connected()) 
-      {
+     while (!client.connected()){
        Serial.print("Attempting MQTT connection ... ");
 
        // Attempt to connect
-       if (client.connect(ARDUINO_CLIENT_ID)) 
-        {
+       if (client.connect(ARDUINO_CLIENT_ID)){
            Serial.println("connected");
           } 
-       else 
-        {
+       else{
            Serial.print("Connection failed, state: ");
            Serial.print(client.state());
            Serial.println(", retrying in 3 seconds");
@@ -116,7 +116,7 @@ void reconectar() //Função que reconecta caso a conexão com o MQTT seja perdi
       }
   }
 
-  void solicitar(){  
+  void solicitar(){                       //Função que solicita dados aos módulos
   unsigned long pausa;
 
   for (byte a = 8; a < 11 ; a++){
@@ -126,79 +126,73 @@ void reconectar() //Função que reconecta caso a conexão com o MQTT seja perdi
     Serial.println("0"+(String)a+"*");
     mySerial.print("0"+(String)a+"*");
     while (pausa > millis()){
-    receberEnviar();  
+    Receber();  
     }
   }
   if (a >= 10){
     Serial.println((String)a+"*");
     mySerial.print((String)a+"*");
     while (pausa > millis()){
-    receberEnviar(); 
+    Receber(); 
     }
   }
  }
 }
 
-void receberEnviar()
-  {
-    
-    
+void Receber(){                          //Função que recebe os dados dos módulos
+          
   //Limpa a variavel mensagem, antes do uso
-     for (byte a = 0; a < 26; a++ )
-      {
+     for (byte a = 0; a < 26; a++ ){
          mensagem[a] = "";
       }
      
      //Recebe dados do buffer até encontrar o caracter * que indica o fim da mensagem          
-     while (mySerial.available() > 0)
-      {
+     while (mySerial.available() > 0){
          bytesRecebidos = mySerial.readBytesUntil('*', mensagem, 26);
         }
+      Publicar();
+  }
 
-      String id_txt = String(mensagem[1])+""+String(mensagem[2]); //cria uma string com os valores contidos em mesagem[1] e mensagem[2]
-      int id_equipe = id_txt.toInt();   //converte a string em int para uso no switch case
-             
-  //Analisa a mensagem recebida e publica no tópico de acordo com a ID da equipe (a ID da equipe está na variavel id_equip)   
-
-     switch (id_equipe)
-      {
-        case 8:
-         client.beginPublish (PUB_8, 26, false); //Inicia a publicação no MQTT
+void Publicar(){                      //Função que publica os dados recebidos
+      if(mensagem[0] == '[' && mensagem[25] == ']'){
+      
+      String id_equipe = String(mensagem[1])+""+String(mensagem[2]); //cria uma string com os valores contidos em mesagem[1] e mensagem[2]
+      int equipe = id_equipe.toInt();   //converte a string em int para uso no switch case
+                   
+         myFile = SD.open("team"+id_equipe+".txt", FILE_WRITE);   //Abre o arquivo para edição
+         if(myFile){
+            Serial.print("Gravando LOG no SD...  ");
+            myFile.println(mensagem);
+            myFile.close();   //Fecha o arquivo após a exibição
+         }
+        Serial.println(mensagem); //Exibe a mensagem na serial (apenas para verificação e teste)
+         switch (equipe){
+          
+         case 8:
+         client.beginPublish ("/app/dados/equipe8", 26, false); //Inicia a publicação no MQTT
          client.print(mensagem); //Publica a mensagem
          client.endPublish(); //Encerra a publicação no MQTT
-         Serial.println(mensagem); //Exibe a mensagem na serial (apenas para verificação e teste)
-         digitalWrite(LED1, HIGH);
-         delay(500);
-         digitalWrite(LED1, LOW);
-         delay(500);
+      
          digitalWrite(LED1, HIGH);
          delay(500);
          digitalWrite(LED1, LOW);
          break;
         
          case 9:
-         client.beginPublish (PUB_9, 26, false);
-         client.print(mensagem);
-         client.endPublish();
-         Serial.println(mensagem);
-         digitalWrite(LED2, HIGH);
-         delay(500);
-         digitalWrite(LED2, LOW);
-         delay(500);
+         client.beginPublish ("/app/dados/equipe9", 26, false); //Inicia a publicação no MQTT
+         client.print(mensagem); //Publica a mensagem
+         client.endPublish(); //Encerra a publicação no MQTT
+     
          digitalWrite(LED2, HIGH);
          delay(500);
          digitalWrite(LED2, LOW);
          break;
      
          case 10:
-         client.beginPublish (PUB_10, 26, false);
-         client.print(mensagem);
-         client.endPublish();
-         Serial.println(mensagem);
-         digitalWrite(LED3, HIGH);
-         delay(500);
-         digitalWrite(LED3, LOW);
-         delay(500);
+         client.beginPublish ("/app/dados/equipe10", 26, false); //Inicia a publicação no MQTT
+         client.print(mensagem); //Publica a mensagem
+         client.endPublish(); //Encerra a publicação no MQTT
+    
          digitalWrite(LED3, HIGH);
          delay(500);
          digitalWrite(LED3, LOW);
@@ -208,3 +202,4 @@ void receberEnviar()
          return;
         }
     }
+  }
