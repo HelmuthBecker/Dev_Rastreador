309c97550be4079344c31b2a1a116455f3886c21
HelmuthBecker <54779926+HelmuthBecker@users.noreply.github.com>
2020-06-27 22:10:01 -0300

    Adicionados Comentários, adaptação as modificações no sketche emissor
    Adicionados alguns comentários para melhor compreensão do código, feitas adaptações para que o sketche fique compativel com o sketche do módulo emissor.

--- RECEPTOR SOLICITA UM POR UM/receber/receber.ino
+++ RECEPTOR SOLICITA UM POR UM/receber/receber.ino
@@ -26,7 +26,11 @@
 #define LED2 8
 #define LED3 7
 
-char mensagem[27];                      //Array de caracteres que irá armazenar a mensagem a ser recebida
+//char mensagem[47];    //mensgem gps + tensao
+//char mensagem[42];                      //Array de caracteres que irá armazenar a mensagem a ser recebida
+char mensagem[32];    //mensagem fake + tensao
+//char mensagem[27];
+
 size_t bytesRecebidos;
 SoftwareSerial mySerial(3,6);           //Rx - Tx
 File myFile;
@@ -36,14 +40,14 @@ EBYTE emissor(&mySerial, 5, 4, 2);
 // Networking details
 byte mac[]    = {  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };  // Ethernet shield (W5100) MAC address
 IPAddress ip(192, 168, 3, 105);                           // Ethernet shield (W5100) IP address
-IPAddress server(192, 168, 3, 114);                       // MTTQ server IP address
+IPAddress server(192, 168, 3, 114);                       // Endereço IP servidor MQTT
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
  
 
 void setup() {
-
+//LEDS (apenas a fins de teste)
    pinMode(LED1, OUTPUT);
    pinMode(LED2, OUTPUT);
    pinMode(LED3, OUTPUT);
@@ -55,7 +59,7 @@ void setup() {
  
    Ethernet.begin(mac, ip);           //Inicia o Ethernet shield
 
-  if (!SD.begin(8)) {
+  if (!SD.begin(8)) {             // 8 representa o pino a ser utilizado pelo sd como CSpin, não utilizar este pino para qualquer outra função
     Serial.println("Erro SD");
     while (1);
   }
@@ -80,17 +84,17 @@ void iniciarLORA ()   //Função que define os parâmetros de configuração do 
      emissor.init();                                            //Inicia o módulo
 
      emissor.SetMode(MODE_NORMAL);                              //Modo de funcionamento do módulo
-     emissor.SetAddressH(0);                                    //Endereço H(?)
-     emissor.SetAddressL(0);                                    //Endereço L(?)
+     emissor.SetAddressH(0);                                    //Endereço H(Não alterar)
+     emissor.SetAddressL(0);                                    //Endereço L(Alterar este valor para trocar o endereço do módulo)
      emissor.SetAirDataRate(ADR_2400);                          //AirDataRate 2400kbps
      emissor.SetUARTBaudRate(UDR_9600);                         //BAUDRate 9600
-     emissor.SetChannel(23);                                    //Canal 23
+     emissor.SetChannel(23);                                    //Canal 23 (Pode variar de 0 até 32)
      emissor.SetParityBit(PB_8N1);                              //Bit Paridade 8N1
      emissor.SetTransmitPower(OPT_TP30);                        //Força de transmissão 30db (Para módulos de 1W/8km)
      //emissor.SetTransmitPower(OPT_TP20);                        //Força de transmissão 20db (Para módulos 100mW/3km)
      emissor.SetWORTIming(OPT_WAKEUP250);                       //WakeUP Time(?) 2000
      emissor.SetFECMode(OPT_FECENABLE);                         //FEC(?) ENABLE
-     emissor.SetTransmissionMode(OPT_FMDISABLE);                //Transmission Mode
+     emissor.SetTransmissionMode(OPT_FMDISABLE);                //Transmission Mode (Fixed or Transparent)
      emissor.SetPullupMode(OPT_IOPUSHPULL);                     //IO Mode PushPull
      emissor.SaveParameters(PERMANENT);                         //Salva as modificações na memória do módulo
          
@@ -116,24 +120,24 @@ void reconectar()                   //Função que reconecta caso a conexão com
       }
   }
 
-  void solicitar(){                       //Função que solicita dados aos módulos
+  void solicitar(){                         //Função que solicita dados aos módulos
   unsigned long pausa;
 
-  for (byte a = 8; a < 11 ; a++){
+  for (byte a = 8; a < 11 ; a++){          //Loop que a cada interação envia o numero da equipe como palavra chave para solicitar informações aos módulos
   pausa = millis() + 3000;
   
-  if (a < 10){
-    Serial.println("0"+(String)a+"*");
+  if (a < 10){                            //Caso o numero da equipe seja menor que 10, adiciona um zero a esquerda como padronização
+    Serial.println("0"+(String)a+"*");    
     mySerial.print("0"+(String)a+"*");
-    while (pausa > millis()){
-    Receber();  
+    while (pausa > millis()){             //Após o envio da solicitação, aguarda 3s (tempo definido na declaração da variável pausa), por uma respota
+    Receber();                            //pela função receber()
     }
   }
-  if (a >= 10){
+  if (a >= 10){                         //Caso o numero da equipe seja maior que 10, não adiciona nada, apenas o "*" de fim de mensagem
     Serial.println((String)a+"*");
     mySerial.print((String)a+"*");
-    while (pausa > millis()){
-    Receber(); 
+    while (pausa > millis()){             //Após o envio da solicitação, aguarda 3s (tempo definido na declaração da variável pausa), por uma respota
+    Receber();                            //pela função receber() 
     }
   }
  }
@@ -142,22 +146,26 @@ void reconectar()                   //Função que reconecta caso a conexão com
 void Receber(){                          //Função que recebe os dados dos módulos
           
   //Limpa a variavel mensagem, antes do uso
-     for (byte a = 0; a < 26; a++ ){
+     for (byte a = 0; a < 31; a++ ){
+     //for (byte a = 0; a < 41; a++ ){
          mensagem[a] = "";
       }
      
      //Recebe dados do buffer até encontrar o caracter * que indica o fim da mensagem          
      while (mySerial.available() > 0){
-         bytesRecebidos = mySerial.readBytesUntil('*', mensagem, 26);
+         bytesRecebidos = mySerial.readBytesUntil('*', mensagem, 31);
+         //bytesRecebidos = mySerial.readBytesUntil('*', mensagem, 41);
         }
       Publicar();
   }
 
 void Publicar(){                      //Função que publica os dados recebidos
-      if(mensagem[0] == '[' && mensagem[25] == ']'){
-      
-      String id_equipe = String(mensagem[1])+""+String(mensagem[2]); //cria uma string com os valores contidos em mesagem[1] e mensagem[2]
-      int equipe = id_equipe.toInt();   //converte a string em int para uso no switch case
+      //if(mensagem[0] == '[' && mensagem[25] == ']'){
+
+      if(mensagem[0] == '[' ){
+        
+      String id_equipe = String(mensagem[1])+""+String(mensagem[2]);      //cria uma string com os valores contidos em mesagem[1] e mensagem[2]
+      int equipe = id_equipe.toInt();                                     //converte a string em int para uso no switch case
                    
          myFile = SD.open("team"+id_equipe+".txt", FILE_WRITE);   //Abre o arquivo para edição
          if(myFile){
